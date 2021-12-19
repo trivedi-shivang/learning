db2a238d6c401b9b2b3033578b9c6e04e2e663bd
Your Name <shivang.softwaredeveloper@gmail.com>
2021-12-19 15:56:02 -0500

    implemented hash-table methods.

---
+++ src/data_structures_and_algorithms/HashTable/HashTable.js
@@ -0,0 +1,59 @@
+import LinkedList from '../LinkedList/LinkedList';
+const defaultHashTableSize = 32;
+
+export default class HashTable {
+    constructor(hashTableSize = defaultHashTableSize) {
+        this.buckets = Array(hashTableSize).fill(null).map(() => new LinkedList());
+        this.keys = {};
+    }
+
+    hash(key) {
+        const hash = Array.from(key).reduce((hashAccumulator, keySymbol) => hashAccumulator += keySymbol.charCodeAt(0), 0);
+
+        // Reduce hash number so it would fit hash table size.
+        return hash % this.buckets.length; 
+    }
+
+    set(key, value) {
+        const keyHash = this.hash(key);
+        this.keys[key] = keyHash;
+        const bucketLinkedList = this.buckets[keyHash];
+        const node = bucketLinkedList.find({callback: (nodeValue) => nodeValue.key === key})
+        if(!node) {
+            bucketLinkedList.append({key, value});
+        }else {
+            node.value.value = value;
+        }
+    }
+
+    delete(key) {
+        const bucketLinkedList = this.buckets[this.keys[key]];
+        delete this.keys[key];
+        const node = bucketLinkedList.find({callback: nodeValue => nodeValue.key === key});
+        if(node) {
+            bucketLinkedList.delete(node.value);
+        }
+    }
+
+    get(key) {
+        const bucketLinkedList = this.buckets[this.keys[key]];
+        const node = bucketLinkedList.find({callback: nodeValue => nodeValue.key === key});
+        return node ? node.value.value : undefined;
+    }
+
+    has(key) {
+        return Object.hasOwnProperty.call(this.keys, key);
+    }
+
+    getKeys() {
+        return Object.keys(this.keys);
+    }
+
+    getValues() {
+        return this.buckets.reduce((values, bucket) => {
+            const bucketValues = bucket.toArray()
+                .map(linkedListNode => linkedListNode.value.value);
+            return values.concat(bucketValues);
+        }, []);
+    }
+}
