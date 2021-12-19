f38e028a70315028e423700ebf8b091ac2b24f7a
Your Name <shivang.softwaredeveloper@gmail.com>
2021-12-19 13:21:24 -0500

    added implementation methods for stack and queue.

--- src/data_structures_and_algorithms/Stack/Stack.js
+++ src/data_structures_and_algorithms/Stack/Stack.js
@@ -1,41 +1,41 @@
 import LinkedList from '../LinkedList/LinkedList';
 
-export default class Queue {
+export default class Stack {
     constructor() {
         this.linkedList = new LinkedList();
     }
 
     isEmpty() {
-        return !this.linkedList.head;
+        return !this.linkedList.tail;
     }
 
     /**
-     * read the element at the front of the queue without removing it
+     * read the element at the end of the stack without removing it
      * Time Complexity: O(1)
      */
     peek() {
         if(this.isEmpty()){
             return null;
         }
-        return this.linkedList.head.value;
+        return this.linkedList.tail.value;
     }
 
     /**
-     * add a new element to the end of the queue (the tail of the linked list).
+     * add a new element to the end of the stack (the tail of the linked list).
      * The element will be procesed after all elements ahead of it.
      * Time Complexity: O(1)
      */
-    enqueue(value) {
+    push(value) {
         this.linkedList.append(value);
     }
 
-    /** remove the element at the front of the queue(the head of the linked list).
-     * If the queue is empty, return null
+    /** remove the element at the end of the stack(the tail of the linked list).
+     * If the stack is empty, return null
      * Time Complexity: O(1)
      */ 
-    dequeue() {
-        const removeHead = this.linkedList.deleteHead();
-        return removeHead ? removeHead.value : null;
+    pop() {
+        const removeTail = this.linkedList.deleteTail();
+        return removeTail ? removeTail.value : null;
     }
 
     toString(callback) {
