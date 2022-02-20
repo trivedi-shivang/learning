95fb771549e8b676244fe87cce081d6914b2da5f
Your Name <shivang.softwaredeveloper@gmail.com>
2022-02-20 11:24:13 -0500

    Added files to contain information about common-interview questions and things-to-consider when solving interview questions.

---
+++ src/data_structures_and_algorithms/CommonQuestions/CommonQuestions.js
@@ -0,0 +1,151 @@
+// O(m+n) O(m)
+function isValidAnagram(s1, s2) {
+  let s1CharFreq = {};
+  if (!s1 || !s2) return;
+  if (s1.length != s2.length) return false;
+  //   traverse s1 characters and store in an object
+  for (let i = 0; i < s1.length; i++) {
+    if (s1CharFreq[s1[i]]) {
+      s1CharFreq[s1[i]] += 1;
+    } else {
+      s1CharFreq[s1[i]] = 1;
+    }
+  }
+
+  //   traverse s2 characters and compare if the characters and their frequencies are same or not
+  for (let i = 0; i < s2.length; i++) {
+    if (!(s2[i] in s1CharFreq)) return false;
+    if (s1CharFreq[s2[i]]) {
+      s1CharFreq[s2[i]] -= 1;
+    }
+  }
+
+  return Object.values(s1CharFreq).every((val) => val === 0);
+}
+
+isValidAnagram("garden", "danger");
+
+// Find an element in a sorted array iteratively
+function findTargetInSortedArray(arr, target) {
+  if (!arr || !target) return;
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (start <= end) {
+    //   search left half of the array
+    if (target < arr[middle]) {
+      end = middle - 1;
+    } else if (target > arr[middle]) {
+      start = middle + 1;
+    } else {
+      return "target found in the array";
+    }
+  }
+  return "target not found in the array";
+}
+
+// Find an element in a sorted array recursively
+function findTargetInSortedArray(arr, target, start, end) {
+  if (!arr || !target) return;
+  start = start || 0;
+  end = end || arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  if (start > end) return "not found";
+  //   search left half of the array
+  if (target === arr[middle]) {
+    return "found";
+  } else {
+    if (target < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    return findTargetInSortedArray(arr, target, start, end);
+  }
+}
+
+findTargetInSortedArray([1, 2, 3, 4, 5], 2);
+
+// find a target in sorted matrix using linear search
+// O(m*n) O(1)
+function targetInMatrix(arr, target) {
+  if (!target || !arr || Array.isArray(arr));
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === target) return "found";
+    }
+  }
+  return "not found";
+}
+
+console.log(targetInMatrix([[1], [2], [3]], 1));
+console.log(targetInMatrix([[1], [2], [3]], 20));
+
+// using binary search
+// O(logm)O(logn) O(1)
+function targetInMatrix(arr, target) {
+  if (!target || !arr || Array.isArray(arr));
+  //   start with the rightmost element.
+  // if the element matches with the target; return
+  // if the element is greater than the target reduce col count
+  // if the element is less than the target increase row count
+  let row = 0;
+  let col = arr.length - 1;
+  while (row <= col) {
+    if (arr[row][col] === target) return "found";
+    else {
+      if (arr[row][col] > target) {
+        col--;
+      } else {
+        row++;
+      }
+    }
+  }
+  return "not found";
+}
+
+console.log(
+  targetInMatrix(
+    [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ],
+    1
+  )
+);
+console.log(
+  targetInMatrix(
+    [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ],
+    20
+  )
+);
+
+// find the symmetric difference
+// https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/find-the-symmetric-difference
+let symDiffObj = {};
+function symDiff(val) {
+  if (val in symDiffObj) {
+    delete symDiffObj[val];
+  } else {
+    symDiffObj[val] = true;
+  }
+}
+function sym(...args) {
+  for (let i = 0; i < args.length; i++) {
+    if (Array.isArray(args[i])) {
+      for (let j = 0; j < args[i].length; j++) {
+        symDiff(args[i][j]);
+      }
+    } else {
+      symDiff(args[i]);
+    }
+  }
+  return Object.keys(symDiffObj).map((val) => parseInt(val));
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
---
+++ src/data_structures_and_algorithms/ThingsToConsider/ThingsToConsider.md
@@ -0,0 +1,20 @@
+# Things to Check for Creating Edge Cases
+
+## Is it an array?
+
+1. Is it even an array? (Array.isArray)
+2. Is it empty or not?
+3. What type of elements does it contain? Of different or same type?
+4. How big is the array?
+
+# Important Conditions to be met
+
+## Binary Search Tree
+
+1. While start<end (where start is the first index of the array and end is the last)
+2. Check if the order of items in the array. Ascending or Descending
+
+## Recursive Function
+
+1. Base Case.
+2. return something from the recursive function.
