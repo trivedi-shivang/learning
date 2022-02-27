a7d08c58ba0d0693d51f78ecb3ce32f6403e4a5e
Your Name <shivang.softwaredeveloper@gmail.com>
2022-02-26 20:38:54 -0500

    Added solution for nth-fibonacci problem

---
+++ src/data_structures_and_algorithms/Algos/AlgoExpert/NthFibonacci.js
@@ -0,0 +1,36 @@
+// O(2^n) Time | O(n) Space (Callstacks)
+function nthFibonacci(n) {
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+  else return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+nthFibonacci(5);
+
+// recursive solution with memoization
+// O(n) Time | O(n) space
+let computedAns = {};
+function nthFibonacci(n) {
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+  if (!(n in computedAns)) {
+    computedAns[n] = nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  }
+  return computedAns[n];
+}
+
+// iteative solution
+// O(n) Time | O(1)
+function nthFibonacci(n) {
+  if (n === 1) return 1;
+  let i = 2;
+  let firstNum = 0;
+  let secondNum = 1;
+  let nthFibonacci;
+  while (i < n) {
+    nthFibonacci = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = nthFibonacci;
+    i++;
+  }
+  return nthFibonacci;
+}
