64bbee3b4b00fe695c242888198053ad83d085cf
Your Name <shivang.softwaredeveloper@gmail.com>
2022-02-27 08:39:00 -0500

    improvised solution by combining conditions into one.

--- src/data_structures_and_algorithms/Algos/AlgoExpert/InsertionSort.js
+++ src/data_structures_and_algorithms/Algos/AlgoExpert/InsertionSort.js
@@ -4,12 +4,8 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let j = i;
-    let isSwapped = false;
-    while (j > 0) {
-      if (arr[j] < arr[j - 1]) {
-        isSwapped = true;
-        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
-      }
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
       j--;
     }
   }
